dc74bd4b5cb6abe559846f17f2fd221f57b1c3fb
Fatuma Hussein <fartunhussein346@gmail.com>
2023-10-19 11:47:04 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -15,7 +15,17 @@ class App
     @rentals = []
     @input = Input.new
   end
-
+  
+def fetch_data(file)
+    if File.exist?("db/#{file}.json")
+      File.read("db/#{file}.json")
+    else
+      empty_json = [].to_json
+      File.write("db/#{file}.json", empty_json)
+      empty_json
+    end
+  end
+  
   def load_data
     books = JSON.parse(fetch_data('books'))
     people = JSON.parse(fetch_data('people'))
